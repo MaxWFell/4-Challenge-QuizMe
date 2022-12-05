34951623320cf7def518c0a60845ea851c1e882b
Max Fell <Maxwilliamfell@gmail.com>
2022-12-05 18:37:30 -0500

    I was being dumb it was score.js file that wasnt posting my scores

--- assets/scores.js
+++ assets/scores.js
@@ -1,25 +1,29 @@
 function printHighscores() {
+
     var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-}
- //sets the highscores in a descending order
- highscores.sort(function(a, b) {
-    return b.score - a.score;
-  });
-
-  highscores.forEach(function(score) {
-    var liTag = document.createElement("li");
-    liTag.textContent = score.initials + " - " + score.score;
-
-    var olEl = document.createElement("highscores");
-    olEl.appendChild(liTag);
-  });
-
-function clearHighscores() {
+  
+    // highscores set in descending order
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+  
+    highscores.forEach(function(score) {
+      // high score tags
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+  
+      // page display
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
+  }
+  
+  function clearHighscores() {
     window.localStorage.removeItem("highscores");
     window.location.reload();
-}
-
-document.getElementById("clear").onclick = clearHighscores;
-
-//makes the function run
- printHighscores();
+  }
+  
+  document.getElementById("clear").onclick = clearHighscores;
+  
+  // run function 
+  printHighscores();
